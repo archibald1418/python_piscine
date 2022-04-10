c81ea6b7ba55b49fb4d0debe1642568b635cdadd
archibald1418 <liam141810@gmail.com>
2022-04-11 00:07:40 +0100

    added ignore-case field check

--- day03/ex03/financial.py
+++ day03/ex03/financial.py
@@ -2,15 +2,28 @@
 
 
 import os, sys
-from xml.dom.minidom import Element
 import requests
 import bs4
 from pprint import pprint
 import pickle
 
 # URL = 'https://finance.yahoo.com/quote/msft/financials?p=msft&guccounter=1'
-URL = 'https://finance.yahoo.com/quote/msft/financials' # TODO: account for tickr
+URL = 'https://finance.yahoo.com/quote/{0}/financials?p={0}' # TODO: account for tickr
 HEADERS = {'User-Agent': 'curl/7.64.1'}
+
+
+def normalize_tickr(tickr):
+    return tickr.lower()
+
+def field_name_ok(field_name, expected_field_name):
+    return hash(field_name.lower()) == hash(expected_field_name.lower())
+
+def check_fields(field, field_names):
+    for field_name in field_names:
+        if field_name_ok(field, field_name):
+            return field_name
+    return None
+    
 
 
 if __name__ == '__main__':
@@ -23,7 +36,8 @@ if __name__ == '__main__':
     
     tickr, field = sys.argv[1:]
     
-    resp = requests.get(url=URL, headers=HEADERS)
+    
+    resp = requests.get(url=URL.format(tickr), headers=HEADERS)
     if resp.status_code == 404:
         raise Exception("URL not found")
     
@@ -43,9 +57,10 @@ if __name__ == '__main__':
     
     all_rows = dict({row[0]: list(row[1:]) for row in all_rows})
     
-    if field not in all_rows:
+    real_field_name = check_fields(field, all_rows.keys())
+    if real_field_name is None:
         raise Exception("Field not found")
     
-    rowValue = tuple([field] + all_rows[field])
+    rowValue = tuple([real_field_name] + all_rows[real_field_name])
     print(rowValue)
     
