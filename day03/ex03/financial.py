b6ce15a3f39020811f6adc4823acd43c7dbe0c80
archibald1418 <liam141810@gmail.com>
2022-04-10 23:48:04 +0100

    parsed tree ok; todo consider tickr

--- day03/ex03/financial.py
+++ day03/ex03/financial.py
@@ -2,14 +2,15 @@
 
 
 import os, sys
+from xml.dom.minidom import Element
 import requests
 import bs4
 from pprint import pprint
+import pickle
 
 # URL = 'https://finance.yahoo.com/quote/msft/financials?p=msft&guccounter=1'
-URL = 'https://finance.yahoo.com/quote/msft/financials'
+URL = 'https://finance.yahoo.com/quote/msft/financials' # TODO: account for tickr
 HEADERS = {'User-Agent': 'curl/7.64.1'}
-
 
 
 if __name__ == '__main__':
@@ -19,9 +20,32 @@ if __name__ == '__main__':
     if argc != 3:
         print("Bad args")
         exit()
-    # try:
+    
+    tickr, field = sys.argv[1:]
+    
     resp = requests.get(url=URL, headers=HEADERS)
     if resp.status_code == 404:
         raise Exception("URL not found")
+    
     soup = bs4.BeautifulSoup(resp.text, 'html.parser')
-    print(soup.prettify())
+    
+    header: bs4.element.Tag = list(
+        map(
+            lambda x: x.contents[0],
+            soup.find("div", {"class": "D(tbhg)"}).findAll('span')))
+    all_rows: bs4.element.Tag  = list(
+        map(
+            lambda x: list(
+                map(
+                    lambda y: y.contents[0], 
+                    x.findAll('span'))),
+            soup.findAll("div", {"data-test":"fin-row"})))
+    
+    all_rows = dict({row[0]: list(row[1:]) for row in all_rows})
+    
+    if field not in all_rows:
+        raise Exception("Field not found")
+    
+    rowValue = tuple([field] + all_rows[field])
+    print(rowValue)
+    
